2f63337857c81d11052ed4647b9199d2bfdfa5e7
Federico <iskorini@gmail.com>
2021-03-14 16:54:58 +0100

    update day6 part1

--- 2020/Day6/part1.py
+++ 2020/Day6/part1.py
@@ -2,7 +2,6 @@ if __name__ == '__main__':
     with open('input.txt', 'r') as f:
         input = f.read()
     groups = input.split('\n\n')
-    print(groups)
     unique_answers = 0
     for group in groups:
         counter = [False]*26
