e5d5186acb00c72802a817bc9715c15053ac30eb
Federico <iskorini@gmail.com>
2021-04-25 16:33:37 +0200

    Day 13 part 2

---
+++ 2020/Day13/part2.py
@@ -0,0 +1,15 @@
+from functools import reduce
+
+def find_schedule(input):
+    N = reduce(lambda x, y: x*y, map(lambda x: x[0], input))
+    Ai = [a[0]-a[1] for a in input]
+    Ni = [N//x for x in map(lambda x: x[0], input)]
+    Yi = [pow(N, -1, n) for N, n in zip(Ni, map(lambda x: x[0], input))] # only for python >= 3.8
+    return sum([a*N*y for a, N, y in zip(Ai, Ni, Yi)]) % N
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        input = f.read()
+    input = [(int(i), index) for index, i in enumerate(input.splitlines()[1].split(',')) if i != 'x']
+    print(find_schedule(input))
